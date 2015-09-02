8c3ad9177f6a1994c450a8ffd31c42a3a0654cd2
Fahien <sir.fahien@gmail.com>
2015-09-02 15:33:32 +0200

    Create a custom view

---
+++ app/src/main/java/me/fahien/android/drawhere/view/BoxDrawingView.java
@@ -0,0 +1,27 @@
+package me.fahien.android.drawhere.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Box Drawing View is a simple custom view
+ *
+ * @author Fahien
+ */
+public class BoxDrawingView extends View {
+
+	/**
+	 * Used when creating the view in code
+	 */
+	public BoxDrawingView(Context context) {
+		this(context, null);
+	}
+
+	/**
+	 * Used when inflating the view from XML
+	 */
+	public BoxDrawingView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+}
